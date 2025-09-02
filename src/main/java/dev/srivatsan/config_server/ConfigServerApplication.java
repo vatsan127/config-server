aca437b49b133054dd18f35241a69a76686be9ea
vatsan127 <vatsan127@gmail.com>
2025-09-02 20:29:27 +0530

    commit for making cloud config server

--- src/main/java/dev/srivatsan/config_server/ConfigServerApplication.java
+++ src/main/java/dev/srivatsan/config_server/ConfigServerApplication.java
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.io.File;
 
 @EnableCaching
+@EnableConfigServer
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class ConfigServerApplication {
